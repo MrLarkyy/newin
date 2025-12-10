6aa30648dc78ae4facb368189f94f60c022e8aeb
yearbarber <66645533+yearbarber@users.noreply.github.com>
2025-12-10 07:58:03 +0300

    build: fix publishing issues

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -37,4 +37,33 @@ configurations {
     }
 }
 
-// we don't need to publish to maven here
+val maven_username = if (env.isPresent("MAVEN_USERNAME")) env.fetch("MAVEN_USERNAME") else ""
+val maven_password = if (env.isPresent("MAVEN_PASSWORD")) env.fetch("MAVEN_PASSWORD") else ""
+
+publishing {
+    repositories {
+        maven {
+            name = "aquaticRepository"
+            url = uri("https://repo.nekroplex.com/releases")
+
+            credentials {
+                username = maven_username
+                password = maven_password
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "lol.farsight"
+            artifactId = "newin"
+            version = "${project.version}"
+            from(components["java"])
+            artifact(tasks.jar) {
+                classifier = "agent"
+            }
+        }
+    }
+}
